defb6dfc29c0fe9c271f6d7cad3eac8215ed1433
francis-tg <franc9885@gmail.com>
2022-09-30 19:50:14 +0000

    release player

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const audios = document.querySelectorAll('.audio-item');
 const audioNode = document.querySelector('audio');
 const playBtn = document.querySelector('.play-pause')
@@ -22,9 +23,10 @@ for (const item in audios) {
     }
 }
 
+document.querySelector("#myVolume").style.width = (audioNode.volume*100)+"%"
+
 playBtn.addEventListener('click', () => {
     // console.log(playBtn.children[0])
-
     if (playBtn.children[0].classList.contains('pause')) {
         audioNode.pause()
         playBtn.innerHTML = `<i class="feather play icon-play"></i>`
@@ -39,7 +41,7 @@ playBtn.addEventListener('click', () => {
 audioNode.addEventListener('timeupdate', () => {
     const progressTime = audioNode.currentTime / audioNode.duration;
     currentDuration = progressTime
-    progress.setAttribute('value', progressTime * 100)
+    progress.style.width =  (progressTime * 100 )+ "%"
 })
 
 
@@ -57,16 +59,18 @@ next.addEventListener('click', () => {
 })
 
 previous.addEventListener('click', () => {
-    progress.setAttribute('value', 0)
-    const audioIndex = parseInt(currentAudio) > 0 ? parseInt(currentAudio) - 1 : 0
-        // console.log(audios[parseInt(currentAudio) + 1].children[1].getAttribute('href'))
-    const audioUrl = audios[audioIndex].children[1].getAttribute('href');
-    // console.log(audioUrl)
-    currentAudio = audioIndex;
-    audioNode.setAttribute('src', '.' + audioUrl)
-    audioNode.play()
-    playBtn.innerHTML = ``
-    playBtn.innerHTML = `<i class="feather pause icon-pause"></i>`
+    if(audioNode.currentTime <= 1){
+        progress.style.width = "0%"
+        const audioIndex = parseInt(currentAudio) > 0 ? parseInt(currentAudio) - 1 : audios.length -1
+            // console.log(audios[parseInt(currentAudio) + 1].children[1].getAttribute('href'))
+        const audioUrl = audios[audioIndex].children[1].getAttribute('href');
+        // console.log(audioUrl)
+        currentAudio = audioIndex;
+        audioNode.setAttribute('src', '.' + audioUrl)
+        audioNode.play()
+        playBtn.innerHTML = ``
+        playBtn.innerHTML = `<i class="feather pause icon-pause"></i>`
+    }else audioNode.currentTime = 0
 })
 
 audioNode.addEventListener('ended', () => {
@@ -84,17 +88,57 @@ audioNode.addEventListener('ended', () => {
     //     playBtn.setAttribute('name', 'play-circle')
     // }
 
+const volumeScrub =(e) =>{
+    const vol = (e.offsetX /document.querySelector('#volume-progressBar').offsetWidth);
+    console.log(vol)
+    audioNode.volume =vol;
+    document.querySelector("#myVolume").style.width = (vol*100)+"%"
+}
+
 const Scrub = (e) => {
-        progress.setAttribute('value', 0)
-        const scrubTime = (e.offsetX / progress.offsetWidth) * audioNode.duration;
-        // setTimeout(() => {
-        progress.setAttribute('value', scrubTime)
+        // progress.s
+        const scrubTime = (e.offsetX / document.querySelector('.progress-bar').offsetWidth) * audioNode.duration;
+        progress.style.width = scrubTime + "%"
         audioNode.currentTime = scrubTime
             // }, 2000)
             // console.log(scrubTime)
     }
     // let mousedown = false;
 
-progress.addEventListener('click', (e) => Scrub(e))
+    document.querySelector('.progress-bar').addEventListener('click', (e) => Scrub(e))
+    document.querySelector('#volume-progressBar').addEventListener('click', (e) => volumeScrub(e))
     // document.querySelector('.progress').addEventListener('mousedown', (e) => mousedown = false)
     // document.querySelector('.progress').addEventListener('mouseup', (e) => mousedown = true)
+const OpenFolder = document.querySelector(".open-folder");
+// OpenFolder.addEventListener("click",async()=>{
+//     await showOpenFilePicker({multiple:true}).then((sf)=>{
+//         const audioTracks = sf;
+//         // document.querySelector(".playlists").innerHTML  = ""
+//         for (const aT in audioTracks) {
+//             if (Object.hasOwnProperty.call(audioTracks, aT)) {
+//                 const tracks = audioTracks[aT];
+//                 // tracks.getFile(tracks.name).then((f)=>{
+//                 //     console.log(URL.createObjectURL(tracks))
+//                 //     window.webkitRequestFileSystem()
+//                 // })
+//                 tracks.move("./")
+                
+//                 // document.querySelector(".playlists").innerHTML  += PendAudioTrack(tracks.name, tracks.name)
+//             }
+//         }
+//     })
+  
+// })
+
+onvolumechange = (e)=>{
+    console.log(e)
+}
+
+function PendAudioTrack(url,name){
+    return `
+    <div class="audio-item">
+    <i class="feather icon-music audio-ico f-30"></i>
+       <a href="${url}" class="item">${name}</a>
+    </div>
+    `
+} 
